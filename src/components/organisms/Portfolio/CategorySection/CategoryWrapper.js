3d7c6e8d5765a57a9dd267e84b8e9cb3a836e690
Oleksandr Speranskyi <sasha.speransky@gmail.com>
2025-11-11 21:32:30 +0200

    refactored CategoryWrapper.js

--- src/components/organisms/Portfolio/CategorySection/CategoryWrapper.js
+++ src/components/organisms/Portfolio/CategorySection/CategoryWrapper.js
@@ -2,13 +2,13 @@ import React from 'react';
 import FirstCategory from '../../../molecules/Portfolio/PortfolioSection/FirstCategory';
 import RealizedInvestment from '../../../molecules/Portfolio/PortfolioSection/RealizedInvestment';
 
-function CategoryWrapper() {
+const CategoryWrapper = () => {
   return (
     <>
       <FirstCategory />
       <RealizedInvestment />
     </>
   );
-}
+};
 
 export default CategoryWrapper;
