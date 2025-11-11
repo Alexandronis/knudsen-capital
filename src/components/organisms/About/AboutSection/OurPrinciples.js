f04d8d99849b1ece92e3d1f5d73c74c55b674cd5
Oleksandr Speranskyi <sasha.speransky@gmail.com>
2025-11-11 20:46:55 +0200

    refactored OurPrinciples.js

--- src/components/organisms/About/AboutSection/OurPrinciples.js
+++ src/components/organisms/About/AboutSection/OurPrinciples.js
@@ -2,17 +2,23 @@ import React from 'react';
 import ourPrinciples1 from '../../../../assets/about-page/our-principles-1.svg';
 import ourPrinciples2 from '../../../../assets/about-page/our-principles-2.svg';
 
-function OurPrinciples() {
+const OurPrinciples = () => {
   return (
     <>
       <div className="sub-heaading">
         <span></span>
         <h2>Our Principles</h2>
       </div>
+
       <div className="common_card">
         <div className="common_card_containar">
           <div className="image_containar">
-            <img className="image_icon" src={ourPrinciples1} alt="KC/LLC Icon" />
+            <img
+              className="image_icon lazy"
+              src={ourPrinciples1}
+              alt="KC/LLC Principle: Profit and Purpose"
+              loading="lazy"
+            />
           </div>
           <div className="leftside_text about_leftside_text">
             <div className="card_list_items about-item">
@@ -27,14 +33,19 @@ function OurPrinciples() {
 
         <div className="common_card_containar oursecond">
           <div className="image_containar">
-            <img className="image_icon" src={ourPrinciples2} alt="KC/LLC Icon" />
+            <img
+              className="image_icon lazy"
+              src={ourPrinciples2}
+              alt="KC/LLC Principle: Integrity and Value"
+              loading="lazy"
+            />
           </div>
           <div className="leftside_text about_leftside_text">
             <div className="card_list_items about-item">
               <div className="item">
                 <p>
-                  We treat people with <b>respect</b>, operate with <b>integrity</b>, and only
-                  invest when we can add <b>real value</b>.
+                  We treat people with <b>respect</b>, operate with <b>integrity</b>, and only invest
+                  when we can add <b>real value</b>.
                 </p>
               </div>
             </div>
@@ -43,6 +54,6 @@ function OurPrinciples() {
       </div>
     </>
   );
-}
+};
 
 export default OurPrinciples;
