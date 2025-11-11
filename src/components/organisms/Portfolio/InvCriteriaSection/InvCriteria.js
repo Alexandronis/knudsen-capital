40f2942b9482e7d84023a37bc40a046fe5bd8b34
Oleksandr Speranskyi <sasha.speransky@gmail.com>
2025-11-11 21:40:12 +0200

    refactored InvCriteria.js

--- src/components/organisms/Portfolio/InvCriteriaSection/InvCriteria.js
+++ src/components/organisms/Portfolio/InvCriteriaSection/InvCriteria.js
@@ -1,75 +1,32 @@
 import React from 'react';
 import greenIcon from '../../../../assets/portfolio-page/green-icon.svg';
+import criteriaList from '../../../../data/invCriteria.json';
 
-function InvCriteria() {
+const InvCriteria = () => {
   return (
-    <>
-      <div className="content-wrapper Criteria">
-        <div className="content-inner-box">
-          <div className="values-title">
-            <span></span>
-            <h2 className="main-title">Investment Criteria</h2>
-          </div>
-          <div className="values-cards">
-            <div className="common_card">
+    <div className="content-wrapper Criteria">
+      <div className="content-inner-box">
+        <div className="values-title">
+          <span></span>
+          <h2 className="main-title">Investment Criteria</h2>
+        </div>
+        <div className="values-cards">
+          {criteriaList.map((text, index) => (
+            <div className="common_card" key={index}>
               <div className="common_card_containar">
                 <div className="leftside_text about_leftside_text">
                   <div className="card_list_items about-item">
                     <img className="image_icon" src={greenIcon} alt="KC/LLC Green Icon" />
-                    <p>Quickly adapted to the new omnichannel business paradigm</p>
+                    <p>{text}</p>
                   </div>
                 </div>
               </div>
             </div>
-
-            <div className="common_card">
-              <div className="common_card_containar">
-                <div className="leftside_text about_leftside_text">
-                  <div className="card_list_items about-item">
-                    <img className="image_icon" src={greenIcon} alt="KC/LLC Green Icon" />
-                    <p>Experiencing exceptional growth rates in today&apos;s climate</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div className="common_card">
-              <div className="common_card_containar">
-                <div className="leftside_text about_leftside_text">
-                  <div className="card_list_items about-item">
-                    <img className="image_icon" src={greenIcon} alt="KC/LLC Green Icon" />
-                    <p>
-                      Effectively capture DTC (own site) + Amazon mastery and solid retail reach
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="common_card">
-              <div className="common_card_containar">
-                <div className="leftside_text about_leftside_text">
-                  <div className="card_list_items about-item">
-                    <img className="image_icon" src={greenIcon} alt="KC/LLC Green Icon" />
-                    <p>Have high gross margins (40%+)</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="common_card">
-              <div className="common_card_containar">
-                <div className="leftside_text about_leftside_text">
-                  <div className="card_list_items about-item">
-                    <img className="image_icon" src={greenIcon} alt="KC/LLC Green Icon" />
-                    <p>Have a high LTV-CAC ratio (&lt;4 and profitable on first order)</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
-    </>
+    </div>
   );
-}
+};
 
 export default InvCriteria;
