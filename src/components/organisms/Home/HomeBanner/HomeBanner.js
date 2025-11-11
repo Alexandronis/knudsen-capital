1e718209183e63e1cca2c6925b3c5d0dc627e87c
Oleksandr Speranskyi <sasha.speransky@gmail.com>
2025-11-11 21:21:31 +0200

    refactored HomeBanner.js

--- src/components/organisms/Home/HomeBanner/HomeBanner.js
+++ src/components/organisms/Home/HomeBanner/HomeBanner.js
@@ -1,40 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactPlayer from 'react-player';
 import mountainVideo from '../../../../assets/home-banner/mountain.mp4';
 import mountainIcon from '../../../../assets/home-banner/mountain-thumbnail.png';
 
-function HomeBanner() {
-  const [isVideoLoaded, setIsVideoLoaded] = React.useState(false);
-  const src = mountainVideo;
+const HomeBanner = () => {
+  const [isVideoLoaded, setIsVideoLoaded] = useState(false);
 
-  const onLoadedData = () => {
+  const handleVideoLoad = () => {
     setIsVideoLoaded(true);
   };
 
   return (
     <div className="home-banner">
-      <img
-        src={mountainIcon}
-        className="video-thumb tiny"
-        alt="KCLLC video thumb"
-        style={{ display: isVideoLoaded ? 'none' : 'block' }}
-      />
+      {!isVideoLoaded && (
+        <img
+          src={mountainIcon}
+          className="video-thumb tiny"
+          alt="KCLLC video thumb"
+          loading="lazy"
+        />
+      )}
       <ReactPlayer
-        url={src}
-        playsinline={true}
-        preload="none"
-        webkit-playsinline="yes"
-        playing={true}
-        loop={true}
-        muted={true}
+        url={mountainVideo}
+        playing
+        loop
+        muted
         controls={false}
         width="100%"
         height="100%"
-        onLoadedData={onLoadedData}
+        playsinline
+        onReady={handleVideoLoad}
         style={{ display: isVideoLoaded ? 'block' : 'none' }}
       />
     </div>
   );
-}
+};
 
 export default HomeBanner;
